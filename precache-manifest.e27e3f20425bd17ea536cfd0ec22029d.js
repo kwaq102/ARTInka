468acee9c6ed153bd3e273c1723b13b14b7129b3
Kamil Gajewski <kwaq102@gmail.com>
2022-10-24 22:47:12 +0200

    Updates

---
+++ precache-manifest.e27e3f20425bd17ea536cfd0ec22029d.js
@@ -0,0 +1,334 @@
+self.__precacheManifest = [
+  {
+    "revision": "41ad59968b5a2dae54d9",
+    "url": "/ARTInka/static/css/main.517ad1a5.chunk.css"
+  },
+  {
+    "revision": "41ad59968b5a2dae54d9",
+    "url": "/ARTInka/static/js/main.503006e5.chunk.js"
+  },
+  {
+    "revision": "c0a2046deaff95fc490d",
+    "url": "/ARTInka/static/js/runtime~main.c1515914.js"
+  },
+  {
+    "revision": "26eab7f19066f71bd17b",
+    "url": "/ARTInka/static/css/2.2057f23f.chunk.css"
+  },
+  {
+    "revision": "26eab7f19066f71bd17b",
+    "url": "/ARTInka/static/js/2.9ad73d20.chunk.js"
+  },
+  {
+    "revision": "81e0d57b6e754dc36c6559a39011b69f",
+    "url": "/ARTInka/static/media/logo.81e0d57b.png"
+  },
+  {
+    "revision": "a267bf79bc7fe1d7ba5b3ebc9ef5e9d7",
+    "url": "/ARTInka/static/media/bubbles.a267bf79.jpg"
+  },
+  {
+    "revision": "15df1af758a04f02e6f0f6d5acf839b9",
+    "url": "/ARTInka/static/media/paintingFace3.15df1af7.jpg"
+  },
+  {
+    "revision": "5a57ad3a3a370d960e96d4bc66c6c210",
+    "url": "/ARTInka/static/media/painting.5a57ad3a.jpeg"
+  },
+  {
+    "revision": "c1784ab356c7842f4cf167294976c1c1",
+    "url": "/ARTInka/static/media/paintFace4.c1784ab3.jpg"
+  },
+  {
+    "revision": "2529f5147e0a2c4696302ca963e906e6",
+    "url": "/ARTInka/static/media/kidsTattoo.2529f514.jpg"
+  },
+  {
+    "revision": "7486d57b0785d700f5905cbeba9e5c16",
+    "url": "/ARTInka/static/media/paintFace_300x300.7486d57b.jpg"
+  },
+  {
+    "revision": "f6690af69ef9215e6d61c7c86497153e",
+    "url": "/ARTInka/static/media/kidsTattoo_300x300.f6690af6.jpg"
+  },
+  {
+    "revision": "f012140cb4d062412e13b10757c49019",
+    "url": "/ARTInka/static/media/braid_300x300.f012140c.jpg"
+  },
+  {
+    "revision": "251f4e6fa5de8c46138ef06619f061c5",
+    "url": "/ARTInka/static/media/crayons_300x300.251f4e6f.jpg"
+  },
+  {
+    "revision": "7c882ea0b700b825f94acf7326936895",
+    "url": "/ARTInka/static/media/balls_300x300.7c882ea0.jpg"
+  },
+  {
+    "revision": "291de2e12b17b81c2c641d5c13679504",
+    "url": "/ARTInka/static/media/bubbles_300x300.291de2e1.jpg"
+  },
+  {
+    "revision": "091a1dd5fe72d84e6d4f85e50c77bb54",
+    "url": "/ARTInka/static/media/baloons_300x300.091a1dd5.jpg"
+  },
+  {
+    "revision": "f4d6b4c23a855b5afa4df993c02f6a1f",
+    "url": "/ARTInka/static/media/laser_300x300.f4d6b4c2.jpg"
+  },
+  {
+    "revision": "27ded1fe4add9e821ce4ffc946de0f3d",
+    "url": "/ARTInka/static/media/phone-solid.27ded1fe.svg"
+  },
+  {
+    "revision": "da9681b2c7a31ddfd3dce82eb913b60a",
+    "url": "/ARTInka/static/media/gallery_normal.da9681b2.jpg"
+  },
+  {
+    "revision": "4fb353058f8c73ed843b1c84e115ff38",
+    "url": "/ARTInka/static/media/gallery_w300.4fb35305.jpg"
+  },
+  {
+    "revision": "3ed75a32ac9f77c8278baa36b90a0871",
+    "url": "/ARTInka/static/media/gallery2_normal.3ed75a32.jpg"
+  },
+  {
+    "revision": "2cc51a506bbee0f9fc01295a05f72d52",
+    "url": "/ARTInka/static/media/gallery2_w300.2cc51a50.jpg"
+  },
+  {
+    "revision": "59053bcbfbc419154c8233fd22221439",
+    "url": "/ARTInka/static/media/gallery3_normal.59053bcb.jpg"
+  },
+  {
+    "revision": "e7f716b0b42df47c6dbaf7ebd88611c3",
+    "url": "/ARTInka/static/media/gallery3_w300.e7f716b0.jpg"
+  },
+  {
+    "revision": "31bdbc848b7db8748d1c7c4a92ec654d",
+    "url": "/ARTInka/static/media/gallery4_normal.31bdbc84.jpg"
+  },
+  {
+    "revision": "41eabaa5f21368d38ad34c196c061cf0",
+    "url": "/ARTInka/static/media/gallery4_w300.41eabaa5.jpg"
+  },
+  {
+    "revision": "4f0fa9d3d572f2d475228d59a61a7f7b",
+    "url": "/ARTInka/static/media/gallery5_normal.4f0fa9d3.jpg"
+  },
+  {
+    "revision": "4b0c33a6b702f327ebbc56d57314264a",
+    "url": "/ARTInka/static/media/gallery5_w300.4b0c33a6.jpg"
+  },
+  {
+    "revision": "4a9213c81fca0d5fa1916d010c56247d",
+    "url": "/ARTInka/static/media/gallery6_normal.4a9213c8.jpg"
+  },
+  {
+    "revision": "edeae6bffce2473975e6b2f4e02c7041",
+    "url": "/ARTInka/static/media/gallery6_w300.edeae6bf.jpg"
+  },
+  {
+    "revision": "6da64ee9f08dbeeff3c1d5168f2e1c29",
+    "url": "/ARTInka/static/media/gallery7_normal.6da64ee9.jpg"
+  },
+  {
+    "revision": "2485d9ce56e6d583751a61caf1f98bb6",
+    "url": "/ARTInka/static/media/gallery7_w300.2485d9ce.jpg"
+  },
+  {
+    "revision": "fbb7d08416b98b9afb9acc9731ce7022",
+    "url": "/ARTInka/static/media/gallery8_normal.fbb7d084.jpg"
+  },
+  {
+    "revision": "d4dc4b1802411ce8e6c3a2562c63fffe",
+    "url": "/ARTInka/static/media/gallery8_w300.d4dc4b18.jpg"
+  },
+  {
+    "revision": "b7bb903d759241b2b72f12146b83f349",
+    "url": "/ARTInka/static/media/gallery9_normal.b7bb903d.jpg"
+  },
+  {
+    "revision": "e630dcf5c406007d2ffed915ccfcae9b",
+    "url": "/ARTInka/static/media/gallery9_w300.e630dcf5.jpg"
+  },
+  {
+    "revision": "787d49370a9662ce834a5d255488e035",
+    "url": "/ARTInka/static/media/gallery10_normal.787d4937.jpg"
+  },
+  {
+    "revision": "b61feb8261cf8a04a36c126add2792e6",
+    "url": "/ARTInka/static/media/gallery10_w300.b61feb82.jpg"
+  },
+  {
+    "revision": "1f8f702e6f6330649dee52b1dd5722d1",
+    "url": "/ARTInka/static/media/gallery11_normal.1f8f702e.jpg"
+  },
+  {
+    "revision": "0cb9a84fed9f43015793eb4c1460d349",
+    "url": "/ARTInka/static/media/gallery11_w300.0cb9a84f.jpg"
+  },
+  {
+    "revision": "8010ab88468a139fb970f4b93440c489",
+    "url": "/ARTInka/static/media/gallery12_normal.8010ab88.jpg"
+  },
+  {
+    "revision": "afe5eecd6818f2a61c18d42cb60a5821",
+    "url": "/ARTInka/static/media/gallery12_w300.afe5eecd.jpg"
+  },
+  {
+    "revision": "293cf96bdd002b2fc6a79eeb5492b682",
+    "url": "/ARTInka/static/media/gallery13_normal.293cf96b.jpg"
+  },
+  {
+    "revision": "cbfdb449804c95ad71453eaa17a48344",
+    "url": "/ARTInka/static/media/gallery13_w300.cbfdb449.jpg"
+  },
+  {
+    "revision": "be6b27c81f370ede024868c751438a23",
+    "url": "/ARTInka/static/media/gallery14_normal.be6b27c8.jpg"
+  },
+  {
+    "revision": "ba3060c9e53811fe93477dfb16034f0f",
+    "url": "/ARTInka/static/media/gallery14_w300.ba3060c9.jpg"
+  },
+  {
+    "revision": "a102314f1637190a2b53d1f8e289c8a6",
+    "url": "/ARTInka/static/media/gallery15_normal.a102314f.jpg"
+  },
+  {
+    "revision": "d1bd679b75ef20e1162a1dcd390b094d",
+    "url": "/ARTInka/static/media/gallery15_w300.d1bd679b.jpg"
+  },
+  {
+    "revision": "2d49687a5f9f1a327b5182cba53bb240",
+    "url": "/ARTInka/static/media/gallery16_normal.2d49687a.jpg"
+  },
+  {
+    "revision": "aeb315de587c135a0a336a48585deb87",
+    "url": "/ARTInka/static/media/gallery16_w300.aeb315de.jpg"
+  },
+  {
+    "revision": "06adbd32bd1eae26f21a5ef59299d45d",
+    "url": "/ARTInka/static/media/gallery17_normal.06adbd32.jpg"
+  },
+  {
+    "revision": "bd3dc8dd7e67bdb9a22f1a9f5a76996d",
+    "url": "/ARTInka/static/media/gallery17_w300.bd3dc8dd.jpg"
+  },
+  {
+    "revision": "bd0ba5150b4e2a515c5ce20cbf7af84e",
+    "url": "/ARTInka/static/media/gallery18_normal.bd0ba515.jpg"
+  },
+  {
+    "revision": "a823d266ee99a5b6f251a37fee19635c",
+    "url": "/ARTInka/static/media/gallery18_w300.a823d266.jpg"
+  },
+  {
+    "revision": "5e77807bd3b7ce1846ece2c0639746c5",
+    "url": "/ARTInka/static/media/gallery19_normal.5e77807b.jpg"
+  },
+  {
+    "revision": "cbb4091c6050793957d499fb2ab9ac00",
+    "url": "/ARTInka/static/media/gallery19_w300.cbb4091c.jpg"
+  },
+  {
+    "revision": "cd9e3af5477451f65b2814746e9bb3c1",
+    "url": "/ARTInka/static/media/justyna.cd9e3af5.jpg"
+  },
+  {
+    "revision": "d5f2a65a6cfbffec6bd6217f482cb110",
+    "url": "/ARTInka/static/media/justyna2.d5f2a65a.jpg"
+  },
+  {
+    "revision": "f247493ce5deab7343babcd765f7d533",
+    "url": "/ARTInka/static/media/justyna3.f247493c.jpg"
+  },
+  {
+    "revision": "b48ea62b740fd615652e73df3dda6956",
+    "url": "/ARTInka/static/media/paintFace2.b48ea62b.jpg"
+  },
+  {
+    "revision": "e438f65d44bb8fe6287b8dba3dc434d6",
+    "url": "/ARTInka/static/media/children.e438f65d.jpg"
+  },
+  {
+    "revision": "039afa3fe7a20831576f457988797cb9",
+    "url": "/ARTInka/static/media/heart.039afa3f.svg"
+  },
+  {
+    "revision": "cb64263e8a42b26f3f42e2fb3ac1b70b",
+    "url": "/ARTInka/static/media/balloon.cb64263e.svg"
+  },
+  {
+    "revision": "05bc829e180b649c2c2f45abe81b3b91",
+    "url": "/ARTInka/static/media/brain.05bc829e.svg"
+  },
+  {
+    "revision": "d8400d23921b0adfaa85044202364e3f",
+    "url": "/ARTInka/static/media/student.d8400d23.svg"
+  },
+  {
+    "revision": "e4aa7caeb73ba5e4919760446be22cc1",
+    "url": "/ARTInka/static/media/c1_w640.e4aa7cae.jpg"
+  },
+  {
+    "revision": "36efa1d9b6315eb5cc2497fcfaaeb589",
+    "url": "/ARTInka/static/media/c2_w640.36efa1d9.jpg"
+  },
+  {
+    "revision": "21e6b0919eb828dd5bdd70d3145a2a05",
+    "url": "/ARTInka/static/media/c3_w640.21e6b091.jpg"
+  },
+  {
+    "revision": "ad1f9264574e5150ab2a48e80fd6313f",
+    "url": "/ARTInka/static/media/c4_w640.ad1f9264.jpg"
+  },
+  {
+    "revision": "8cb430b60bcf3b873a2f1e698d34efc2",
+    "url": "/ARTInka/static/media/c5_w640.8cb430b6.jpg"
+  },
+  {
+    "revision": "e7932e77da264a8fc2eebcb0d85b7829",
+    "url": "/ARTInka/static/media/c6_w640.e7932e77.jpg"
+  },
+  {
+    "revision": "3ca132f112624a96ef7120a0b4bc7d14",
+    "url": "/ARTInka/static/media/c7_w640.3ca132f1.jpg"
+  },
+  {
+    "revision": "758d65557e197db447c6c1067399c69c",
+    "url": "/ARTInka/static/media/c8_w640.758d6555.jpg"
+  },
+  {
+    "revision": "ccbe2f8a02859a1760aeea7e7ebd2ed5",
+    "url": "/ARTInka/static/media/c9_w640.ccbe2f8a.jpg"
+  },
+  {
+    "revision": "be124f021eb8b779b5d68c30a7f3ea36",
+    "url": "/ARTInka/static/media/phone-solid-white.be124f02.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ARTInka/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ARTInka/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ARTInka/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ARTInka/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "8d859cce2ebb159f8a09538aea93145a",
+    "url": "/ARTInka/static/media/the-sun.8d859cce.png"
+  },
+  {
+    "revision": "d439545554741b5e509a205d0eebb5fb",
+    "url": "/ARTInka/index.html"
+  }
+];
